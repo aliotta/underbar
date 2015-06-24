99f7325d0b54c94c67b56085dd64f7c4437f75b5
Alexander Liotta <aliotta343@gmail.com>
2015-06-24 16:44:52 -0700

    Revert ""Testing from a different computer to see change tracking""
    This reverts commit 35055151aa50de85a07c55ee6766d4ac267fcef2.

--- src/underbar.js
+++ src/underbar.js
@@ -66,7 +66,6 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
-	  
   };
 
   // Return all elements of an array that don't pass a truth test.
